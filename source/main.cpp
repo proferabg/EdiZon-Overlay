9fe723fd576ea7998229fbc50e1c0244d206722e
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2025-12-13 23:53:57 -0800

    Update main.cpp - warning message centering fix

--- source/main.cpp
+++ source/main.cpp
@@ -48,7 +48,7 @@ public:
     virtual tsl::elm::Element* createUI() {
         auto *rootFrame = new tsl::elm::HeaderOverlayFrame();
         rootFrame->setHeader(new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
-            renderer->drawString(APP_TITLE, false, 20, 50, 32, (tsl::defaultOverlayColor));
+            renderer->drawString("EdiZon", false, 20, 50, 32, (tsl::defaultOverlayColor));
             renderer->drawString(APP_VERSION, false, 20, 52+23, 15, (tsl::bannerVersionTextColor));
 
             if (edz::cheat::CheatManager::getProcessID() != 0) {
@@ -110,21 +110,23 @@ public:
     }
     ~GuiCheats() { }
 
+
     virtual tsl::elm::Element* createUI() override {
         auto rootFrame = new tsl::elm::HeaderOverlayFrame(97);
 
-        bool setOnce = true; // for ensuring header sync with frame caching for header overlayframe
+       // bool setOnce = true; // for ensuring header sync with frame caching for header overlayframe
 
-        rootFrame->setHeader(new tsl::elm::CustomDrawer([this, &setOnce](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
-            renderer->drawString(APP_TITLE, false, 20, 50, 32, (tsl::defaultOverlayColor));
+        rootFrame->setHeader(new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, s32 x, s32 y, s32 w, s32 h) {
+            renderer->drawString("EdiZon", false, 20, 50, 32, (tsl::defaultOverlayColor));
 
             //static bool runOnce = true;
-            if (setOnce) {
-                renderer->drawString(APP_VERSION, false, 20, 52+23, 15, (tsl::bannerVersionTextColor));
-                setOnce = false;
-            } else {
-                renderer->drawString("Cheats", false, 20, 52+23, 15, (tsl::bannerVersionTextColor));
-            }
+            //if (setOnce) {
+            //    renderer->drawString(APP_VERSION, false, 20, 52+23, 15, (tsl::bannerVersionTextColor));
+            //    setOnce = false;
+            //} else {
+            //    renderer->drawString("Cheats", false, 20, 52+23, 15, (tsl::bannerVersionTextColor));
+            //}
+            renderer->drawString("Cheats", false, 20, 52+23, 15, (tsl::bannerVersionTextColor));
             
 
             if (edz::cheat::CheatManager::getProcessID() != 0) {
@@ -139,8 +141,10 @@ public:
 
         if (edz::cheat::CheatManager::getCheats().size() == 0) {
             auto warning = new tsl::elm::CustomDrawer([](tsl::gfx::Renderer *renderer, u16 x, u16 y, u16 w, u16 h){
-                renderer->drawString("\uE150", false, 180, 274, 90, (0xFFFF));
-                renderer->drawString("No Cheats loaded!", false, 110, 360, 25, (0xFFFF));
+                static const auto iconWidth = renderer->getTextDimensions("\uE150", false, 90).first;
+                static const auto textWidth = renderer->getTextDimensions("No Cheats loaded!", false, 25).first;
+                renderer->drawString("\uE150", false, (tsl::cfg::FramebufferWidth - iconWidth) / 2, 274, 90, (0xFFFF));
+                renderer->drawString("No Cheats loaded!", false, (tsl::cfg::FramebufferWidth - textWidth) / 2, 360, 25, (0xFFFF));
             });
 
             rootFrame->setContent(warning);
@@ -200,7 +204,7 @@ public:
                         replaceAll(cheatNameCheck, ":ENABLED", "");
 
                         auto cheatToggleItem = new tsl::elm::ToggleListItem(/*formatString("%d:%s: %s", cheat->getID(), (cheat->isEnabled() ? "y" : "n"),*/ cheatNameCheck/*.c_str()).c_str()*/, cheat->isEnabled());
-                        cheatToggleItem->setStateChangedListener([&cheat](bool state) { cheat->setState(state); });
+                        cheatToggleItem->setStateChangedListener([&cheat](bool state) { cheat->setState(state);});
 
                         this->m_cheatToggleItems.insert({cheat->getID(), cheatToggleItem});
                         list->addItem(cheatToggleItem);
@@ -227,8 +231,10 @@ public:
             // display if no cheats in submenu
             if(this->m_numCheats < 1){
                 auto warning = new tsl::elm::CustomDrawer([](tsl::gfx::Renderer *renderer, u16 x, u16 y, u16 w, u16 h){
-                    renderer->drawString("\uE150", false, 180, 250, 90, (0xFFFF));
-                    renderer->drawString("No Cheats in Submenu!", false, 110, 340, 25, (0xFFFF));
+                    static const auto iconWidth = renderer->getTextDimensions("\uE150", false, 90).first;
+                    static const auto textWidth = renderer->getTextDimensions("No Cheats in Submenu!", false, 25).first;
+                    renderer->drawString("\uE150", false, (tsl::cfg::FramebufferWidth - iconWidth) / 2, 250, 90, (0xFFFF));
+                    renderer->drawString("No Cheats in Submenu!", false, (tsl::cfg::FramebufferWidth - textWidth) / 2, 340, 25, (0xFFFF));
                 });
 
                 rootFrame->setContent(warning);
@@ -285,7 +291,7 @@ public:
      }
             
     virtual tsl::elm::Element* createUI() override {
-        auto rootFrame = new tsl::elm::OverlayFrame(APP_TITLE, "System Information");
+        auto rootFrame = new tsl::elm::OverlayFrame("EdiZon", "System Information");
     
         auto infos = new tsl::elm::CustomDrawer([this](tsl::gfx::Renderer *renderer, u16 x, u16 y, u16 w, u16 h){
     
@@ -369,8 +375,6 @@ public:
                 renderer->drawString("WiFi Signal:", false, 63, 400, 18, (tsl::style::color::ColorText));
                 renderer->drawString(formatString("%d dBm", signalStrength).c_str(), false, 258, 400, 18, (tsl::style::color::ColorHighlight)); 
             }
-            renderer->drawString("Credits:", false, 63, 600, 18, (tsl::style::color::ColorText));
-            renderer->drawString(APP_AUTHOR, false, 75, 630, 18, (tsl::style::color::ColorHighlight)); 
         });
         rootFrame->setContent(infos);
     
